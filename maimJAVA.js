c178816734c9b44077d0c4bb076a777d11d0cf44
Callandor-droid <draxhenn@kkg-do.eu>
2025-06-24 19:50:59 +0200

    javascript initialized

---
+++ maimJAVA.js
@@ -0,0 +1,6 @@
+function start () {
+    //new text
+    document.getElementById("story").innerHTML ="Du stehst vor der Tür der Schule, es klingelt und alle anderen gehen rein. <br> was tust du?";
+    document.getElementById("optiona").innerHTML="<button> Ich gehe mit allen anderen rein. </button>";
+    document.getElementById("optionb").innerHTML="<button> Ich bleibe drausen. </button>"
+}
